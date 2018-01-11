4056da0e43a95f7c8c85624a83681cbb47b9ca0a
MoondanceZ <Qyk_zZ@hotmail.com>
2018-01-11 09:20:16 +0800

    Update: add transition layout

--- Vue-Account/src/components/Layout.vue
+++ Vue-Account/src/components/Layout.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>
-    <router-view></router-view>
-  </div>
+<transition mode="out-in">
+  <router-view></router-view>
+</transition>
 </template>
 <script>
 export default {
-  name: "layout",
-  data() {
-    return {};
-  },
-  methods: {}
+  name: "layout"
 };
+</script>
+<style scoped>
+.left-enter {
+  transform: translateX(100%);
+}
+.left-enter-to {
+  transform: translateX(0);
+}
+
+.left-enter-active {
+  transition: 1s;
+}
+.left-leave {
+  transform: translateX(0);
+}
+.left-leave-to {
+  transform: translateX(-100%);
+}
+.left-leave-active {
+  transition: 1s;
+}
+</style>
