5218f09ad4536401d191bf145a379917c4b17785
MoondanceZ <Qyk_zZ@hotmail.com>
2018-01-31 17:48:57 +0800

    UPdate

--- Vue-Account/src/components/Layout.vue
+++ Vue-Account/src/components/Layout.vue
@@ -8,28 +8,38 @@
 </template>
 
 <script>
-export default {
-};
+export default {};
 </script>
 
 <style scoped>
-.slide-fade{
+.layout {
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+.slide-fade {
   position: absolute;
   width: 100%;
   height: 100%;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
   /* overflow-y: auto; */
   /* overflow-x: hidden; */
   -webkit-overflow-scrolling: touch;
 }
 .slide-fade-enter-active {
-  transition: all .8s ease;
+  transition: all 0.8s ease;
 }
 .slide-fade-leave-active {
-
-  transition: all .8s cubic-bezier(2.0, 0.5, 0.8, 1.0);
+  transition: all 0.8s cubic-bezier(2, 0.5, 0.8, 1);
 }
-.slide-fade-enter, .slide-fade-leave-to
-{
+.slide-fade-enter,
+.slide-fade-leave-to {
   /* left:0;right: 0; */
   transform: translate3d(100%, 0, 0);
   opacity: 1;
